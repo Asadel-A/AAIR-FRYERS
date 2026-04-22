8963b287508985edb1f02d6b1fd8b0e2af193fb3
RoshandilAzeemi <roshandil45@gmail.com>
2026-04-22 16:01:35 -0400

    css fixed for admin dashboard

--- client2/admin_dashboard.php
+++ client2/admin_dashboard.php
@@ -24,6 +24,7 @@ $lastImport = $pdo->query("SELECT imported_at FROM import_log ORDER BY imported_
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel | MEJ</title>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
