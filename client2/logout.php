9290f3b6b4e5d2bd51f8ee7e394e88e1835da4ca
Asadel Ali <asadelali07@gmail.com>
2026-04-22 04:47:29 -0400

    Update logout.php

--- client2/logout.php
+++ client2/logout.php
@@ -1,3 +1,10 @@
+<!--
+Name: Roshan Azeemi
+Date: March 16 2026
+Description: Logs the user out of the website and destroys their link to the login.
+-->
+
+
 <?php
 session_start();
 session_destroy();
